24a84248cc3bfce597a254445e37ad130afae322
joe <joetheguy@msn.com>
2017-04-22 23:07:42 -0700

    yo

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jdub on 10/04/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        P2_EvenFibNumbers question = new P2_EvenFibNumbers();
+        System.out.println(question.getSolution());
+    }
+}
