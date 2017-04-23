ceab492f23038ac8eb928f1a2f12cc7f716d8d15
joe <joetheguy@msn.com>
2017-04-22 23:09:25 -0700

    Up to date

---
+++ P1_MultiplesOfThreeAndFive.java
@@ -0,0 +1,31 @@
+/**
+ * Created by jdub on 10/04/17.
+ * <p>
+ * Multiples of 3 and 5
+ * Problem 1
+ * <p>
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+public class P1_MultiplesOfThreeAndFive implements Solution{
+
+
+    private static int inConstantTime() {
+
+        return 0;
+    }
+
+    private static int bruteForce(){
+        int sum = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0)
+                System.out.println(sum += i);
+        }
+        return sum;
+    }
+
+    @Override
+    public Object getSolution() {
+        return new String(String.valueOf(bruteForce()));
+    }
+}
